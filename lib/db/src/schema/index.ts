e578032d61bbd44a39b437c1561e09c0d6fc819c
Dionatan Gomes <dionatangomes08@gmail.com>
2026-06-28 10:53:45 -0300

    Ajuste Instalação Windows

--- lib/db/src/schema/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./users";
-export * from "./companies";
-export * from "./company_users";
-export * from "./structures";
-export * from "./reservations";
-export * from "./plans";
-export * from "./subscriptions";
